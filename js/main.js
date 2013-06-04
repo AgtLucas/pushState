e5f395ced2b397bcf4de205c4b39d74bed161135
Lucas <metallucas.ds@gmail.com>
2013-06-04 20:01:47 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function() {
 
-    var $main = $(".main");
+    var main = $(".main");
 
     $(document).on("click", "a", function() {
         var href = $("this").attr("href");
 
         history.pushState({}, '', href);
-        $main.load(href + " .main>*");
+        main.load(href + " .main>*");
         return false;
     });
 
