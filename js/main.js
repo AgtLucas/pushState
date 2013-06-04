dc9772d9962d52999b515145512b779e39b2fa12
Lucas <metallucas.ds@gmail.com>
2013-06-04 19:35:26 -0300

    Update jQuery

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    var $main = $(".main");
+
+    $(document).on("click", "a", function() {
+        var href = $("this").attr("href");
+
+        history.pushState({}, '', href);
+        $main.load(href + " .main>*");
+        return false;
+    });
+
+});
