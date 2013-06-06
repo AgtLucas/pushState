d7c2e6c025da98e62693bb0604ec22057ce93468
Lucas <metallucas.ds@gmail.com>
2013-06-05 22:36:19 -0300

    Update main js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,26 @@ $(function() {
         var newHash      = "",
             $mainContent = $(".main"),
             $el;
+
+        $("header").delegate("a", "click", function() {
+            _link = $(this).attr("href");
+            history.pushState(null, null, _link);
+            loadContent(_link);
+            return false;
+        });
+
+        function loadContent(href) {
+            $mainContent
+                    .find(".wrap")
+                    .fadeOut(200, function() {
+                        $mainContent.hide().load(href + ".wrap");
+                    });
+        }
+
+        $(window).bind('popstate', function() {
+            _link = location.pathname.replace(/^.*[\\\/]/, '');
+            loadContent(_link);
+        });
     }
 
 });
