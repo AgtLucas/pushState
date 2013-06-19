17cb742a210c26855207e6b44a8190ed7551c9d0
Lucas <metallucas.ds@gmail.com>
2013-06-18 22:52:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 (function() {
 
+    function supports_history_api() {
+        return !!(window.history && history.pushState);
+    }
+
     function setupHistoryClicks() {
         addClicker(document.getElementById("home"));
         addClicker(document.getElementById("about"));
     }
+
+    function
 
 })();
