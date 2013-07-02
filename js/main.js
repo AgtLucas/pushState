100cc13286ee668b3bc498d6437d876029d82daa
Lucas <metallucas.ds@gmail.com>
2013-07-01 21:59:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-$(function() {
+// $(function() {
 
-    if(Modernizr.history) {
+//     if(Modernizr.history) {
 
-        var newHash      = "",
-            $mainContent = $(".main"),
-            $el;
+//         var newHash      = "",
+//             $mainContent = $(".main"),
+//             $el;
 
-        $("header").delegate("a", "click", function() {
-            _link = $(this).attr("href");
-            history.pushState(null, null, _link);
-            loadContent(_link);
-            return false;
-        });
+//         $("header").delegate("a", "click", function() {
+//             _link = $(this).attr("href");
+//             history.pushState(null, null, _link);
+//             loadContent(_link);
+//             return false;
+//         });
 
-        function loadContent(href) {
-            $mainContent
-                    .find(".wrap")
-                    .fadeOut(1000, function() {
-                        $mainContent.hide().load(href + " .wrap", function() {
-                            $mainContent.fadeIn(400);
-                        });
-                    });
-        }
+//         function loadContent(href) {
+//             $mainContent
+//                     .find(".wrap")
+//                     .fadeOut(1000, function() {
+//                         $mainContent.hide().load(href + " .wrap", function() {
+//                             $mainContent.fadeIn(400);
+//                         });
+//                     });
+//         }
 
-        $(window).bind('popstate', function() {
-            _link = location.pathname.replace(/^.*[\\\/]/, '');
-            loadContent(_link);
-        });
-    }
+//         $(window).bind('popstate', function() {
+//             _link = location.pathname.replace(/^.*[\\\/]/, '');
+//             loadContent(_link);
+//         });
+//     }
 
-});
+// });
